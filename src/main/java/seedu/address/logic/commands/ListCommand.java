8ce552c80d77a519c4685acec3014c71dfb657f9
ryanlimdx <ryanlimdx@gmail.com>
2024-03-23 17:34:53 +0800

    Update ListCommand and Parser to search tags

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -1,10 +1,11 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import seedu.address.model.Model;
+import seedu.address.model.person.TagContainsKeywordsPredicate;
 
 /**
  * Lists all persons in the address book to the user.
@@ -15,12 +16,21 @@ public class ListCommand extends Command {
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Lists relevant contacts from the address book. "
             + "Parameters: "
-            + "[" + PREFIX_WARD + "WARD]...\n"
+            + "[" + PREFIX_TAG + "WARD]...\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_WARD + "3 ";
+            + PREFIX_TAG + "Fall-risk ";
+
+    private final TagContainsKeywordsPredicate predicate;
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
 
+    public ListCommand(TagContainsKeywordsPredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    public ListCommand() {
+        this.predicate = null;
+    }
 
     @Override
     public CommandResult execute(Model model) {
