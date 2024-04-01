ddbf058e02d071078705415b0b4541d801ed1f39
erv-teo <107994607+erv-teo@users.noreply.github.com>
2024-04-01 22:15:04 +0800

    Add testcases for Remark

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -1,5 +1,6 @@
 package seedu.address.model.person;
 
+import static java.util.Objects.requireNonNull;
 /**
  * Represents a Person's remarks in the address book.
  */
@@ -12,6 +13,7 @@ public class Remark {
      * @param value A valid remark.
      */
     public Remark(String value) {
+        requireNonNull(value);
         this.value = value;
     }
 
