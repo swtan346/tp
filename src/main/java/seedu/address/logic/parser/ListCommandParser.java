b42dd89162f4d55f92f5b3961da3930f2fbf13ab
ryanlimdx <ryanlimdx@gmail.com>
2024-04-12 00:36:25 +0800

    Update form of javadoc comment in ListCommandParser

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -20,9 +20,9 @@ public class ListCommandParser implements Parser<ListCommand> {
      * Parses the given {@code String} of arguments in the context of the ListCommand
      * and returns a ListCommand object for execution.
      *
-     * @throws ParseException if the user input does not conform the expected format
      * @param args the arguments to parse.
      * @return a ListCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
      */
     public ListCommand parse(String args) throws ParseException {
         String trimmedArgs = args.trim();
