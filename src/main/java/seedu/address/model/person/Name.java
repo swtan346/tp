7acba8bfc0336a42b65d4f5dff704dd80954209a
iamtr <jettzeong@gmail.com>
2024-04-12 13:29:20 +0800

    Add brackets

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -11,7 +11,7 @@ public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Names should only contain alphanumeric characters and spaces, "
-                    + "and only these special characters (' ， / -), "
+                    + "and only these special characters (\" ' \"  ,  \" , \"  ,  \" / \"  ,  \" - \"), "
                     + "and it should not be blank";
     /*
      * The first character of the address must not be a whitespace,
